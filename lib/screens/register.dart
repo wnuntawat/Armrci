3bb10b3033fd0211d8781b299ab90746db381a22
masterARM <wnuntawat@gmail.com>
2019-10-10 13:27:17 +0700

    DisplayName ok

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,5 @@
 import 'package:armrci/screens/my_style.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -74,8 +75,56 @@ class _RegisterState extends State<Register> {
         print('You Click Upload');
         formKey.currentState.save();
         print('name=$nameString,email=$emailString,password=$passwordString');
+        registerThread();
       },
     );
+  }
+
+  Future<void> registerThread() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    await firebaseAuth
+        .createUserWithEmailAndPassword(
+            email: emailString, password: passwordString)
+        .then((response) {
+      print('Register Success');
+      setUpDisplayName();
+    }).catchError((response) {
+      String title = response.code;
+      String message = response.message;
+      print('title=$title,message=$message');
+      myAlert(title, message);
+    });
+  }
+
+  Future<void> setUpDisplayName() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    await firebaseAuth.currentUser().then((response) {
+      UserUpdateInfo userUpdateInfo =UserUpdateInfo();
+      userUpdateInfo.displayName = nameString;
+      response.updateProfile(userUpdateInfo);
+
+
+      
+    });
+  }
+
+  void myAlert(String title, String message) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(title),
+            content: Text(message),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
 
   @override
